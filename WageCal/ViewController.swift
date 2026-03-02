07433006571b9b8631f820bbf939ea2263429a72
Stacy Ramirez Llamas <ramirezllamas570@flagler.edu>
2026-03-01 20:04:54 -0500

    Initial Commit

---
+++ WageCal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WageCal
+//
+//  Created by Ramirez Llamas, Stacy on 3/1/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
